cd850526be07701cfa42786c04daa3ece000be56
Git-DH <kdh9345@naver.com>
2022-07-15 10:59:34 +0900

    기준 날짜 변경

--- src/views/BoxOffice.vue
+++ src/views/BoxOffice.vue
@@ -2,12 +2,11 @@
   <div>
     <h1>{{ title }}</h1>
     <div>
-      <input type="date" v-model="selectedDate">
+      <input type="date" v-model="selectedDate" />
       <button @click="search">검색</button>
-    </div>    
+    </div>
     <rank-table :list="list" />
   </div>
-
 </template>
 
 <script>
@@ -22,7 +21,8 @@ export default {
             title: '',
             selectedDate: '',
             list: [],
-            pathName: ''
+            pathName: '',
+            changeDate: ''
         }
     },
     methods: {
@@ -32,28 +32,44 @@ export default {
         },
         async getData(targetDt) {
             switch(this.pathName) {
-                case 'boxOfficeByDay':                    
+                case 'boxOfficeByDay':
                     this.list = (await this.getBoxOfficeByDay(targetDt))
                                 .boxOfficeResult.dailyBoxOfficeList;
                     this.search();
                     break;
-                case 'boxOfficeByWeek':                    
+                case 'boxOfficeByWeek':
                     this.list = (await this.getBoxOfficeByWeek(targetDt))
                                 .boxOfficeResult.weeklyBoxOfficeList;
                     this.search();
                     break;
             }
         },
-        
+
     },
-    created() {    
+    created() {
         const d = new Date();
-        d.setDate(d.getDate() - 1);
+        if(this.pathName === 'boxOfficeByDay') {
+            d.setDate(d.getDate() - 1);
+        } else {
+            d.setDate(d.getDate() - 7);
+        }
+        
         this.selectedDate = this.getOnlyDateStr(d);
+        switch(this.pathName) {
+            case 'boxOfficeByDay':
+                this.changeDate = d.setDate(d.getDate() - 1);
+                this.selectedDate = this.getOnlyDateStr(d);
+                break;
+            case 'boxOfficeByWeek':
+                this.changeDate = d.setDate(d.getDate() - 7);
+                this.selectedDate = this.getOnlyDateStr(d);
+                break;       
+        } 
     },
     updated() {
+        const d = new Date();
         this.pathName = this.$route.name; // index.js의 route사용
-        
+
         switch(this.pathName) {
             case 'boxOfficeByDay':
                 this.title = '일별 박스오피스';
@@ -62,11 +78,20 @@ export default {
                 this.title = '주간 박스오피스';
                 break;
         }
-        
+
+        switch(this.pathName) {
+            case 'boxOfficeByDay':
+                this.changeDate = d.setDate(d.getDate() - 1);
+                this.selectedDate = this.getOnlyDateStr(d);
+                break;
+            case 'boxOfficeByWeek':
+                this.changeDate = d.setDate(d.getDate() - 7);
+                this.selectedDate = this.getOnlyDateStr(d);
+                break;       
+        }
+
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
